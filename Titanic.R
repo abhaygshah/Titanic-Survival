353ac746ecf8b9eaa2f649c0ad36bfbd159c029e
abhaygshah <50455967+abhaygshah@users.noreply.github.com>
2019-05-09 08:33:43 -0700

    Update Titanic.R

--- Titanic.R
+++ Titanic.R
@@ -1,3 +1,9 @@
 train = read.csv("Desktop/Titanic/train.csv")
 test = read.csv("Desktop/Titanic/test.csv")
 
+names(train)
+[1] "PassengerId" "Survived"    "Pclass"      "Name"       
+[5] "Sex"         "Age"         "SibSp"       "Parch"      
+[9] "Ticket"      "Fare"        "Cabin"       "Embarked"
+
+View(train)
